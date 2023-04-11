de77b6b6957a7a25ec6ec48b1320577270a37dac
JZasJam <jonathan.zasada-james@student.shu.ac.uk>
2023-04-12 00:05:32 +0100

    Updated with additional sensor data

---
+++ messageProcessor.js
@@ -0,0 +1,45 @@
+/*
+* IoT Hub Raspberry Pi NodeJS - Microsoft Sample Code - Copyright (c) 2017 - Licensed MIT
+*/
+'use strict';
+
+const Bme280Sensor = require('./bme280Sensor.js');
+const SimulatedSensor = require('./simulatedSensor.js');
+const SenseHatSensor = require('./senseHatSensor.js');
+
+function MessageProcessor(option) {
+  option = Object.assign({
+    deviceId: '[Unknown device] node',
+    temperatureAlert: 30
+  }, option);
+  //this.sensor = option.simulatedData ? new SimulatedSensor() : new Bme280Sensor(option.i2cOption);
+  this.sensor = option.simulatedData ? new SimulatedSensor() : new SenseHatSensor();
+  this.deviceId = option.deviceId;
+  this.temperatureAlert = option.temperatureAlert
+  this.sensor.init(() => {
+    this.inited = true;
+  });
+}
+
+MessageProcessor.prototype.getMessage = function (messageId, cb) {
+  if (!this.inited) { return; }
+  this.sensor.read((err, data) => {
+    if (err) {
+      console.log('[Sensor] Read data failed due to:\n\t' + err.message);
+      return;
+    }
+
+    cb(JSON.stringify({
+      messageId: messageId,
+      deviceId: this.deviceId,
+      temperature: data.temperature,
+      humidity: data.humidity,
+      pressure: data.pressure,
+      compass: data.compass,
+      accel: data.accel,
+      gyro: data.gyro
+    }), data.temperature > this.temperatureAlert);
+  });
+}
+
+module.exports = MessageProcessor;
---
+++ senseHatSensor.js
@@ -0,0 +1,73 @@
+'use strict';
+
+
+const util = require('util')
+const senseHat  = require('node-sense-hat');
+const imu = senseHat.Imu;
+const IMU = new imu.IMU();
+
+
+function Sensor(/* options */) {
+  // nothing todo
+}
+
+
+Sensor.prototype.init = function (callback) {
+  // nothing todo
+  callback();
+}
+
+
+Sensor.prototype.read = function (callback) {
+  IMU.getValue
+ 
+    ( (e, data) => {
+      
+      //console.log("DATA" +  JSON.stringify(data))
+      
+      /*
+      data.temperature = data.temperature.toFixed(4) ;
+      data.humidity = data.humidity.toFixed(4);
+      console.log("console log humd: " + data.humidity);
+      console.log("console log temp: " + data.temperature);
+      */
+
+      data.temperature = data.temperature.toFixed(4);
+      data.humidity = data.humidity.toFixed(4);
+      data.pressure = data.pressure.toFixed(4);
+      
+      data.compass = {
+        x: data.compass.x.toFixed(4),
+        y: data.compass.y.toFixed(4),
+        z: data.compass.z.toFixed(4)
+      };
+      data.accel = {
+        x: data.accel.x.toFixed(4),
+        y: data.accel.y.toFixed(4),
+        z: data.accel.z.toFixed(4)
+      };
+      data.gyro = {
+        x: data.gyro.x.toFixed(4),
+        y: data.gyro.y.toFixed(4),
+        z: data.gyro.z.toFixed(4)
+      };
+      
+
+      console.log("Temperature: " + data.temperature);
+      console.log("Humidity: " + data.humidity);
+      
+      console.log("Pressure: " + data.pressure);
+      console.log("Compass: " + JSON.stringify(data.compass));
+      console.log("Accelerometer: " + JSON.stringify(data.accel));
+      console.log("Gyroscope: " + JSON.stringify(data.gyro));
+      
+
+
+      callback(null, data);
+    })
+   
+   // .catch(callback);
+}
+
+
+module.exports = Sensor;
